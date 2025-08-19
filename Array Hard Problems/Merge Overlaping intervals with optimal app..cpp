d54ab5ba4e20c852374c43fa7d1cd441690b016a
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-19 12:01:23 +0530

    changes in this codes

--- Array Hard Problems/Merge Overlaping intervals with optimal app..cpp
+++ Array Hard Problems/Merge Overlaping intervals with optimal app..cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	
+	vector<vector<int> > intervals; 
+	intervals.push_back({1, 3}); 
+	intervals.push_back({2, 6}); 
+	intervals.push_back({8, 9}); 
+	intervals.push_back({9, 11}); 
+	intervals.push_back({8, 10}); 
+	intervals.push_back({2, 4}); 
+	intervals.push_back({15, 18}); 
+	intervals.push_back({16, 17});
+	int n=intervals.size();
+	sort(intervals.begin(),intervals.end());
+	vector<vector<int> > ans;
+	for(int i=0;i<n;i++){
+		if(ans.empty() || intervals[i][0] > ans.back()[1]){
+			ans.push_back(intervals[i]);
+		}
+		else{
+			ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+		}
+	}
+	for(int i=0;i<ans.size();i++){
+		cout<<"["<<ans[i][0]<<","<<ans[i][1]<<"]\n";
+	}
 }
--- Array Hard Problems/Merge overlapping intervals.cpp
+++ Array Hard Problems/Merge overlapping intervals.cpp
@@ -16,7 +16,7 @@ int main(){
     for(int i=0;i<n;i++){
     	int start=arr[i][0];
     	int end=arr[i][1];
-    	if(!ans.empty()&& end<=ans.back()[1]){
+    	if(!ans.empty() || end<=ans.back()[1]){
     		continue;
 		}
 		for(int j=i+1;j<n;j++){
