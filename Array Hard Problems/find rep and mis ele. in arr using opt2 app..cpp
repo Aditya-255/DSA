428b2b266bb5b36a1286d7e4f0e44152c709810f
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-20 18:17:20 +0530

    give optimal solution using XOR

--- Array Hard Problems/find rep and mis ele. in arr using opt2 app..cpp
+++ Array Hard Problems/find rep and mis ele. in arr using opt2 app..cpp
@@ -3,4 +3,49 @@ using namespace std;
 int main(){
 	int n=6;
 	int arr[n]={4,3,6,2,2,1};
+	int xr=0;
+	for(int i=0;i<n;i++){
+		xr=xr^arr[i];
+		xr=xr^(i+1);
+	}
+	int bitno=0;
+	while(1){
+		if((xr&(1<<bitno))!=0){
+			break;
+		}
+		bitno++;
+	}
+	int z=0;
+	int o=0;
+	for(int i=0;i<n;i++){
+		if((arr[i]&(1<<bitno))!=0){
+			o=o^arr[i];
+		}
+		else{
+			z=z^arr[i];
+		}
+	}
+	for(int i=1;i<=n;i++){
+		if((i&(1<<bitno))!=0){
+			o=o^i;
+		}
+		else{
+			z=z^i;
+		}
+	}
+	int cnt=0;
+	for(int i=0;i<n;i++){
+		if(arr[i]==z) cnt++;
+	}
+	int rep=-1;
+	int mis=-1;
+	if(cnt==2){
+		 rep=z;
+		 mis=o;
+	}
+	else{
+		rep=o;
+		mis=z;
+	}
+	cout<<rep<<" "<<mis<<endl;
 }
