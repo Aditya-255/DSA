d4463f615ec53649355fc65be7609a1caf16d3bd
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-20 16:13:59 +0530

    create merged two array

---
+++ Array Hard Problems/Merge Sorted array without extra space using brute app..cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n=4;
+	int m=5;
+	int arr1[n]={1,3,5,7};
+	int arr2[m]={0,2,6,8,9};
+	int arr3[n+m];
+	int l=0;
+	int r=0;
+	int i=0;
+	while(l<n && r<m){
+		if(arr1[l]<=arr2[r){
+			arr3[i]=arr1[l];
+			l++;
+			i++;
+		}
+		else{
+			arr3[i]=arr2[r]
+			r++;
+			i++;
+		}
+	}
+	while(i<n){
+		arr3[i]=arr1[l];
+		i++;
+		l++;
+	}
+	while(r<m){
+		arr3[i]=arr2[r];
+		i++;
+		r++;
+	}
+	for(int i=0;i<n+m;i++){
+		if(i<n){
+			arr1[i]=arr3[i];
+		}
+		else{
+			arr2[i-n]=arr3[i];
+		}
+	}
+}
