ee92747661cd298f34af51ff4aba829092209330
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-23 19:00:23 +0530

    changes in optimal solution

--- Binary search on 1D array/Find first and last occurence of x using optmal sol..cpp
+++ Binary search on 1D array/Find first and last occurence of x using optmal sol..cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int lowerbound(vector<int>& nums,int n,int x){
 	int low=0;int high= n-1;
-	int ans=n;
+	int ans=n; 
 	while(low<=high){
 		int mid=(low+high)/2;
 		if(nums[mid]>=x){
