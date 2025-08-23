7c17f069d7c9f1cd7720a56a75621f6cbbc666fb
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-23 17:18:49 +0530

    create optimal solution for problem

---
+++ Binary search on 1D array/Find first and last occurence of x using optmal sol..cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerbound(vector<int>& nums,int n,int x){
+	int low=0;int high= n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(nums[mid]>=x){
+			ans=mid;
+			high=mid-1;
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+int upperbound(vector<int>& nums,int n,int x){
+	int low=0,high=n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(nums[mid]>x){
+			ans=mid;
+			high=mid-1;;
+		}
+		low=mid+1;
+	}
+	return ans;
+}
+int main(){
+	int n=8;
+	vector<int> nums;
+	nums.push_back(2);
+	nums.push_back(4);
+	nums.push_back(6);
+	nums.push_back(8);
+	nums.push_back(8);
+	nums.push_back(8);
+	nums.push_back(9);
+	nums.push_back(10);
+	int x=8;
+	vector<int> ans;
+	ans.push_back(-1);
+	ans.push_back(-1);
+	int lb=lowerbound(nums,n,x);
+	int ub=upperbound(nums,n,x);
+	if(lb==n || nums[lb]!=x){
+		for(int i=0;i<n;i++){
+			ans[i]=-1;
+		}
+	}
+	ans[0]=lb;
+	ans[1]=ub-1;
+	cout<<lb<<" "<<ub-1<<endl;
+}
