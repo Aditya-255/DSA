49c890a27d16dda58d7eda05b6987c6e6690bfa0
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-23 16:49:22 +0530

    create solution for first and last occ. of x problem

--- Binary search on 1D array/First ans last occurence of x.cpp
+++ Binary search on 1D array/First ans last occurence of x.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int n=7;
-	
+	int n=9;
+	int arr[n]={2,4,6,8,8,8,9,11,13};
+	int x=8;
+	int fir=-1,las=-1;
+	for(int i=0;i<n;i++){
+		if(arr[i]==x){
+			if(fir==-1){
+				fir=i;
+			}
+			las=i;
+		}
+	}
+	cout<<fir<<" "<<las<<endl;
 }
