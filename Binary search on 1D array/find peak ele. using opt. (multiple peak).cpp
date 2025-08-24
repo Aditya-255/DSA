f6b92b3eabc7a5659091f9598515d631c3e3b06e
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-24 17:58:41 +0530

    create find peak ele. problem in mul. peak

--- Binary search on 1D array/find peak ele. using opt. (multiple peak).cpp
+++ Binary search on 1D array/find peak ele. using opt. (multiple peak).cpp
@@ -1,9 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int Peak(int nums[],int n){
-	if(n==1){
-		return 0;
-	}
+int Peak1(int nums[],int n){
+	if(n==1) return 0;
 	if(nums[0]>nums[1]){
 		return 0;
 	}
@@ -22,12 +20,15 @@ int Peak(int nums[],int n){
 		else if(nums[mid]>nums[mid+1]){
 			high=mid-1;
 		}
+		else{
+			low=mid+1;
+		}
 	}
 	return -1;
 }
 int main(){
 	int n=10;
 	int nums[n]={1,2,3,4,5,6,7,8,5,1};
-	int ans=Peak(nums,n);
+	int ans=Peak1(nums,n);
 	cout<<ans<<endl;
 }
