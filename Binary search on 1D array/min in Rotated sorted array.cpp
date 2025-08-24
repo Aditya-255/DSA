8e5bc6d95f807b3ff454f4bca1a6a65a1b5fda11
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-24 11:01:49 +0530

    changes in this codes

--- Binary search on 1D array/min in Rotated sorted array.cpp
+++ Binary search on 1D array/min in Rotated sorted array.cpp
@@ -1,5 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	
+	int n=5;
+	int nums[n]={4,5,1,2,3};
+	int ans=INT_MAX;
+	int low=0,high=n-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(nums[low]<=nums[mid]){
+			ans=min(ans,nums[low]);
+			low=mid+1;
+		}
+		else{
+			ans=min(ans,nums[mid]);
+			high=mid-1;
+		}
+	}
+	cout<<ans<<endl;
 }
