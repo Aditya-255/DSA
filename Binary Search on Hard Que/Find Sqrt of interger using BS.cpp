05fe6faea97c97014465eeb5ba3321ed4457155f
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-25 17:00:34 +0530

    create find Sqrt of int using BS

---
+++ Binary Search on Hard Que/Find Sqrt of interger using BS.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>;
+using namespace std;
+int main(){
+	int n=36;
+	int low=1,high=n;
+	int ans=1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(mid*mid<=n){
+			ans=mid;
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}	
+	cout<<ans<<endl;
+}
