a939b9feee1a1d3d23fee4e411e4b6916ccdf042
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-26 19:13:41 +0530

    create optimal solution for BS problem

---
+++ Binary Search on Hard Que/min day to make M bouquest using BS.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool possible(int bloomDay[],int day,int m,int k){
+	int cnt=0,booke=0;
+	int n=8;
+	for(int i=0;i<n;i++){
+		if(bloomDay[i]<=day){
+		cnt++;
+		}
+		else{
+			booke+=(cnt/k);
+			cnt=0;
+		}
+	}
+	booke+=(cnt/k);
+	if(booke>=m){
+		return true;
+	}
+	return false;
+}
+int minii(int bloomDay[],int n){
+	int mini=INT_MAX;
+	for(int i=0;i<n;i++){
+		if(bloomDay[i]<mini){
+			mini=bloomDay[i];
+		}
+	}
+	return mini;
+}
+int maxii(int bloomDay[],int n){
+	int maxi=INT_MIN;
+	for(int i=0;i<n;i++){
+		if(bloomDay[i]>maxi){
+			maxi=bloomDay[i];
+		}
+	}
+	return maxi;
+}
+int BSmin(int bloomDay[],int m,int k){
+	int n=8;
+	int maxi=INT_MIN;
+	int mini=INT_MAX;
+	for(int i=0;i<n;i++){
+		mini=min(mini,bloomDay[i]);
+		maxi=max(maxi,bloomDay[i]);
+	}
+	int low=mini;
+	int high=maxi;
+	int ans=-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(possible(bloomDay,mid,m,k)){
+			ans=mid;
+			high=mid-1;
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+int main(){
+	int n=8;
+	int bloomDay[n]={7,7,7,7,13,11,12,7};
+	int m=2,k=3;
+	int ans=BSmin(bloomDay,m,k);
+	cout<<ans<<endl;
+}
