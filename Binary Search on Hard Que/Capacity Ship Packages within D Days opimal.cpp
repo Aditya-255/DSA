15fa83777bfc4b96d1af059c277fc184de8ad0cd
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-26 21:17:08 +0530

    create 	 Capacity to Ship Packages within D Days using optimal

--- Binary Search on Hard Que/Capacity Ship Packages within D Days opimal.cpp
+++ Binary Search on Hard Que/Capacity Ship Packages within D Days opimal.cpp
@@ -1,5 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+int Days(int weights[],int cap,int n){
+	int day=1,load=0;
+	for(int i=0;i<n;i++){
+		if(load+weights[i]>cap){
+			day++;
+			load=weights[i];
+		}
+		else{
+			load+=weights[i];
+		}
+	}
+	return day;
+}
+int leastcapacity(int weights[],int n,int day){
+	int ans=-1;
+	int maxi=INT_MIN;
+	for(int i=0;i<n;i++){
+		maxi=max(maxi,weights[i]);
+	}
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=weights[i];
+	}
+	int low=maxi,high=sum;
+	while(low<=high){
+		int mid=(low+high)/2;
+		int noofdays=Days(weights,mid,n);
+		if(noofdays<=day){
+			ans=mid;
+			high=mid-1;
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
 int main(){
-	
+	int n=10;
+	int weights[n]={1,2,3,4,5,6,7,8,9,10};
+	int day=5;
+	int ans=leastcapacity(weights,n,day);
+	cout<<ans<<endl;
 }
