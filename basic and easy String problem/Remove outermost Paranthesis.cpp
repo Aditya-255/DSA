6685529175757955d36b6019d898f503187bf1d4
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-09-02 16:45:26 +0530

    create remove outer parathesis in string

---
+++ basic and easy String problem/Remove outermost Paranthesis.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeOuterParentheses(string s) {
+    string result = "";
+    int depth = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            if (depth > 0) {
+                result += s[i]; 
+            }
+            depth++;
+        } else { 
+            depth--;
+            if (depth > 0) {
+                result += s[i]; 
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    string s = "(()())(())(()(()))";
+    cout << removeOuterParentheses(s); 
+}
+
