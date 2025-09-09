23e3f64301994c9283662439d1fc26292e338f26
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-09-09 15:32:02 +0530

    create reverse LL using stack

---
+++ Medium problems in LL/Reverse LL.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+	public:
+		int data;
+		Node* next;
+		Node* back;
+		Node(int d){
+			data=d;
+			next=NULL;
+			back=NULL;
+		}
+};
+Node* Convert(vector<int> &arr){
+	Node* head=new Node(arr[0]);
+	Node* move=head;
+	for(int i=1;i<arr.size();i++){
+		Node* temp=new Node(arr[i]);
+		temp->back=move;
+		move->next=temp;
+		move=temp;
+	}
+	return head;
+}
+Node* rev(Node* head){
+	Node* temp=head;
+	stack<int> st;
+	while(temp!=NULL){
+		st.push(temp->data);
+		temp=temp->next;
+	}
+	temp=head;
+	while(temp!=NULL){
+		temp->data=st.top();
+		st.pop();
+		temp=temp->next;
+	}
+	return head;
+}
+int main(){
+	vector<int> arr;
+	arr.push_back(4);
+	arr.push_back(2);
+	arr.push_back(3);
+	arr.push_back(1);
+	Node* head=Convert(arr);
+	head=rev(head);
+	Node* temp=head;
+	while(temp!=NULL){
+		cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
