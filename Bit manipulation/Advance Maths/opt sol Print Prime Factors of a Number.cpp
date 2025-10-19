636422773ea2efda6130a6caaa893ecb96470bce
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-10-19 17:15:10 +0530

    optimal sol for prime factors

---
+++ Bit manipulation/Advance Maths/opt sol Print Prime Factors of a Number.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	vector<int> lst;
+	int n=35s;
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0){
+			lst.push_back(i);
+			while(n%i==0){
+				n=n/i;
+			}
+		}
+	}
+	if(n!=1){
+		lst.push_back(n);
+	}
+	for(int i=0;i<lst.size();i++){
+		cout<<lst[i]<<" ";
+	}
+}
