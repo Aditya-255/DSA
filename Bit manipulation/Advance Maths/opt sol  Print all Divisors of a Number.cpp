ec58b9d1d0dba8eabf71400fe904310339207aa6
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-10-19 17:37:54 +0530

    optimal  Print all Divisors of a Number

---
+++ Bit manipulation/Advance Maths/opt sol  Print all Divisors of a Number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n=36;
+	vector<int> ans;
+	for(int i=1;i*i<=n;i++){
+		if(n%i==0){
+			ans.push_back(i);
+			if(n/i!=i){
+				ans.push_back(n/i);
+			}
+		}
+	}
+	for(int i=0;i<ans.size();i++){
+		cout<<ans[i]<<" ";
+	}
+}
