7efe1863c6ce35e646e6d7e9668b89d102ed7ee4
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-10-26 11:13:19 +0530

    Smallest Prime Factor (SPF)

---
+++ Bit manipulation/Advance Maths/Smallest Prime Factor.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n=30;
+	int spf[n+1];
+	for(int i=1;i<=n;i++){
+		spf[i]=i;
+	}
+	for(int i=2;i*i<=n;i++){
+		if(spf[i]==i){
+			for(int j=i*i;j<=n;j+=i){
+				if(spf[j]==j){
+					spf[j]=i;
+				}
+			}
+		}
+	}
+	while(n!=1){
+		cout<<spf[n]<<" ";
+		n=n/spf[n];
+	}
+}
