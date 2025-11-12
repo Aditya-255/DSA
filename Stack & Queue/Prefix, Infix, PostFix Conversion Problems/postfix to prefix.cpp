7a721998bf2f7223be5c9195daebec415c9ed40a
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-11-12 12:10:33 +0530

    create postfix to prefix

---
+++ Stack & Queue/Prefix, Infix, PostFix Conversion Problems/postfix to prefix.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+string posttopre(string s){
+	int i=0;
+	stack<string> st;
+	string ans="";
+	while(i<s.length()){
+		if((s[i]>='A'&&s[i]<='Z')
+		|| (s[i]>='a'&&s[i]<='z')
+		|| (s[i]>='0' &&s[i]<='9')){
+			string temp(1,s[i]);
+			st.push(temp);
+		}
+		else{
+			string t1=st.top();
+			st.pop();
+			string t2=st.top();
+			st.pop();
+			st.push(s[i]+t2+t1);
+		}
+		i++;
+	}
+	return st.top();
+}
+int main(){
+	string s="AB-DE+F*/";
+	string ans=posttopre(s);
+	cout<<ans<<endl;
+}
