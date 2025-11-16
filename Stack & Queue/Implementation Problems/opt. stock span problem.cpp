b62ee2adba5ece08e95001164d1d0584b564222e
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-11-16 20:01:20 +0530

    create optimal solution

---
+++ Stack & Queue/Implementation Problems/opt. stock span problem.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int stockspan(int arr[],int ind,int n){
+	int c=0;
+	stack<int> st;
+	int a=-1;
+	for(int i=0;i<=ind;i++){
+		while(!st.empty()&& arr[st.top()]<=arr[i]){
+			st.pop();
+		}
+		int span;
+		if(st.empty()){
+			span=i+1;
+		}
+		else{
+			span=i-st.top();
+		}
+		st.push(i);
+		if(i==ind){
+			return span;
+		}
+	}
+	return 1;
+}
+int main(){
+	int n=7;
+	int arr[n]={7,2,1,3,3,1,8};
+	int ans=stockspan(arr,4,n);
+	cout<<ans<<endl;
+}
